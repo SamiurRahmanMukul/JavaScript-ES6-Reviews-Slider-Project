5891c341e6da8d3abaa1bafa357511b3be5c7807
Md. Samiur Rahman Mukul <sr.mukul9090@gmail.com>
2021-02-20 09:34:39 +0600

    ES6 Reveiews Silder Build

---
+++ assets/js/data.js
@@ -0,0 +1,23 @@
+const people = [
+  {
+    img: "../img/mukul_pic.png",
+    name: "samiur rahman mukul",
+    job: "as a programmer & developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img: "../img/alam_pic.jpg",
+    name: "soliman hossain alam",
+    job: "android app developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img: "../img/arif_pic.jpg",
+    name: "ariful islam jibon",
+    job: "dear friends me",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+];
+
+// TODO: default export module
+export default people;
