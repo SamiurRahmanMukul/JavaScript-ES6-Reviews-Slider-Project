8937e839bad6037e9d0349547cdac1d0d2ac7775
Md. Samiur Rahman Mukul <sr.mukul9090@gmail.com>
2021-02-20 09:45:33 +0600

    Image Bug Fixed Problem Solved

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,18 +1,24 @@
 const people = [
   {
-    img: "../img/mukul_pic.png",
+    // img: "../img/mukul_pic.png",
+    img:
+      "https://scontent.fdac12-1.fna.fbcdn.net/v/t1.0-9/93426676_694607031295481_4243160966683951104_o.jpg?_nc_cat=109&ccb=3&_nc_sid=09cbfe&_nc_ohc=kUrXgSgPHhoAX8dj7qn&_nc_ht=scontent.fdac12-1.fna&oh=5df58b00841bf1fd06a470f859bf46dc&oe=604D11D2",
     name: "samiur rahman mukul",
     job: "as a programmer & developer",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
   },
   {
-    img: "../img/alam_pic.jpg",
+    // img: "../img/alam_pic.jpg",
+    img:
+      "https://scontent.fdac12-1.fna.fbcdn.net/v/t1.0-9/s851x315/137292919_241876404013302_2999018909202772308_o.jpg?_nc_cat=108&ccb=3&_nc_sid=da31f3&_nc_ohc=uOGoGIXHulAAX92psQS&_nc_ht=scontent.fdac12-1.fna&tp=7&oh=f94732a3fc3b65b346875062b65f86c3&oe=604E1EDE",
     name: "soliman hossain alam",
     job: "android app developer",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
   {
-    img: "../img/arif_pic.jpg",
+    // img: "../img/arif_pic.jpg",
+    img:
+      "https://scontent.fdac12-1.fna.fbcdn.net/v/t1.0-9/96711729_1176429069374815_49443929799524352_n.jpg?_nc_cat=100&ccb=3&_nc_sid=8bfeb9&_nc_ohc=PAfHPrVkSf4AX_dNINX&_nc_ht=scontent.fdac12-1.fna&oh=5f4daef330e71ed8bd41b6c4a374b70a&oe=604DDFA9",
     name: "ariful islam jibon",
     job: "dear friends me",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
